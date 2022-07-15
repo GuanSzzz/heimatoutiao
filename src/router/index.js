69ca611b0c99cdd8fe91c206d539378b2d3c408a
GuanSzzz <847388139@qq.com>
2022-07-15 17:56:06 +0800

    完成了layout布局

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,35 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/',
+    redirect: '/ '
+  },
+
+  {
     path: '/login',
     component: () => import('@/views/Login')
+  },
+  {
+    path: '/',
+    component: () => import('@/views/Layout'),
+    children: [
+      {
+        path: '/ ',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: '/video',
+        component: () => import('@/views/Video')
+      },
+      {
+        path: '/qa',
+        component: () => import('@/views/QA')
+      },
+      {
+        path: '/profile',
+        component: () => import('@/views/My')
+      }
+    ]
   }
 ]
 
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <van-tabbar route>
+      <van-tabbar-item icon="home-o" to="/ ">
+        <span>首页</span>
+        <template #icon>
+          <i class="toutiao toutiao-shouye"></i>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item icon="home-o" to="/video">
+        <span>视频</span>
+        <template #icon>
+          <i class="toutiao toutiao-shipin"></i>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item icon="home-o" to="/qa">
+        <span>问答</span>
+        <template #icon>
+          <i class="toutiao toutiao-wenda"></i>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item icon="home-o" to="/profile">
+        <span>我的</span>
+        <template #icon>
+          <i class="toutiao toutiao-wode"></i>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="less">
+:deep(.van-tabbar-item) {
+  font-size: 0.32rem;
+  .toutiao {
+    font-size: 0.5333rem;
+  }
+}
+</style>
